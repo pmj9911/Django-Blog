50d5f0a6744e05d0edeaa1740c9ebfc51412894d
pmj9911 <parthjardosh@gmail.com>
2019-05-24 15:13:59 +0530

    temp

---
+++ BlogApp/ArticlesApp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Article
+# Create your views here.
+def article_list(request):
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'ArticlesApp/articles_list.html',{'articles':articles})
