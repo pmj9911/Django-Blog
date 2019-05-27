ab4378255c23266eebb3d61006bdb9bd3df66172
pmj9911 <parthjardosh@gmail.com>
2019-05-27 12:57:31 +0530

    api basic done

--- BlogApp/ArticlesApp/models.py
+++ BlogApp/ArticlesApp/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     thumb =  models.ImageField(default='default.png',blank=True)
     author = models.ForeignKey(User, default=None, on_delete=models.PROTECT)
-
+    document = models.FileField(upload_to='documents/',default='default.txt')         
     def __str__(self):
         return self.title
 
